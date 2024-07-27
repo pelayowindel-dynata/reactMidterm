0fccccad542d247203cfb6193ad89f0f2c2c1a9d
pelayowindel-dynata <windel.pelayo@dynata.com>
2024-07-27 14:24:56 +0800

    ...

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+const App = () => {
+    return ( 
+        <div className="parent">
+            <h1>sdf</h1>
+            <div className="btn-click">
+                Click
+            </div>
+        </div>
+     );
+}
+ 
+export default App;
